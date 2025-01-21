ddb1cf11d6eb4ba6128ac81a6f03996c2c174045
Davidsusairaj s <davidsusairaj.sebasthiyar@solverminds.com>
2025-01-21 10:11:33 +0530

    email_JS added

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -25,19 +25,19 @@ const Contact = () => {
     setLoading(true);
     setCurrentAnimation("hit");
 
-    // emailjs
-    //   .send(
-    //    "service_dbx32ae",
-    //    "template_ft6clcj",
-    //     {
-    //       from_name: form.name,
-    //       to_name: "JavaScript Mastery",
-    //       from_email: form.email,
-    //       to_email: "sujata@jsmastery.pro",
-    //       message: form.message,
-    //     },
-    //     import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
-    //   )
+    emailjs
+      .send(
+       import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+       import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        {
+          from_name: form.name,
+          to_name: "David susairaj",
+          from_email: form.email,
+          to_email: "davidsusairaj1996@gmail.com",
+          message: form.message,
+        },
+        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+      )
       // .then(
       //   () => {
       //     setLoading(false);
