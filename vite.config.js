3ef5c0e3261ce91265087d9a980d7be90258a85a
Davidsusairaj s <davidsusairaj.sebasthiyar@solverminds.com>
2025-02-20 10:35:52 +0530

    path changes

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.glb'],  // ✅ Ensure .glb files are included
-  base: '/',  // ✅ Ensures correct base URL in production
+  base: './',  // ✅ Ensures correct base URL in production
   build: {
     outDir: 'dist', // ✅ Vite will output files to dist/
   },
